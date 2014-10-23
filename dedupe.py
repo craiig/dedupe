68d4ea50425d27009f3a871f89e14e2db3e90a56
craig <craiig@gmail.com>
2014-10-22 20:28:26 -0700

    updated

--- dedupe.py
+++ dedupe.py
@@ -10,7 +10,7 @@ csvfile = csv.reader(open(filename), delimiter=',')
 header = csvfile.next() #read first line which is the header
 
 for row in csvfile:
-	print "---- First record"
+	print "---- Record"
 	print row
 	print row[0]
 	print row[1]
