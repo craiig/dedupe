15b558aaa4913bf4def37fb4eedc8ec0c21779a3
craig <craiig@gmail.com>
2014-10-28 19:48:12 -0700

    added tags and header support to merge

--- merge.py
+++ merge.py
@@ -1,7 +1,8 @@
 import sys
 
 overwrite_columns = dict()
-def merge_record(a, b):
+tag_column = dict()
+def merge_record(a, b, header=None):
 	#set up dictionary to remember the columns to overwrite
 	#if not hasattr(merge_record, 'overwrite_columns'):
 	#	merge_record.overwrite_columns = dict()
@@ -18,11 +19,15 @@ def merge_record(a, b):
 				new[i] = b[i]
 			elif new[i] == b[i]:
 				pass #nothing to do because they already match!
-			elif i not in overwrite_columns:
-				print "Both records have a value in column %d, which one do you want to keep?" % (i)
+			elif i not in overwrite_columns and i not in tag_column:
+				if header != None:
+					print "Both records have a value in column \"%s\", which one do you want to keep?" % (header[i])
+				else: 
+					print "Both records have a value in column %d, which one do you want to keep?" % (i)
 				print "[A]: " + new[i]
 				print "[B]: " + b[i]
-				print "[F] Always keep first seen value (never will ask you about this column again)"
+				print "[F]: Always keep first seen value (never will ask you about this column again)"
+				print "[T]: Merge this column as the tag column"
 
 				done = False
 				while not done:
@@ -30,19 +35,33 @@ def merge_record(a, b):
 					if choice == "A":
 						print "Using value from A: %s" % new[i]
 						done = True
+					if choice =="T":
+						tag_column[i] = 1
+						done = True
 					if choice == "B":
 						new[i] = b[i]
 						print "Using value from B: %s" % b[i]
 						done = True
-					elif choice == "F":
+					if choice == "F":
 						#remember this column should never be asked again
-						overwrite_columns[i] = 1;
+						overwrite_columns[i] = 1
 						print overwrite_columns
 						print "Ok, always keeping first value for column %d" % i
 						done = True
+
+			if i in tag_column:
+				#parse column as tag
+				new[i] = map(lambda x: x.strip(), new[i].split(","))
+				print "Tag column parsed:"
+				print "A was parsed as " + str(new[i])
+				print "B was parsed as " + str(  map(lambda x: x.strip(), b[i].split(",")))
+				new[i] = ", ".join(set(new[i] + map( lambda x: x.strip(), b[i].split(",")))) #sorry sorry sorry
+
 	return new
 
-a = ['x','y', '', '10', "33"]
-b = ['x','y', '20', '', "44"]
-print merge_record(a, b)
-print merge_record(a, b)
+#testing code
+#header = ["one", "two", "three", "four", "five", "tags"]
+#a = ['x','y', '', '10', "666", "12, 34,67,11"]
+#b = ['x','y', '20', '', "44", "89, 10, 11, 12"]
+#print merge_record(a, b, header)
+#print merge_record(a, b)
